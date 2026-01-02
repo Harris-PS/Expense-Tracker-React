0e8586a6f14228fc830451656167e7befe89d291
Harris-PS <psharris2002@gmail.com>
2026-01-02 14:37:00 +0530

    Changed the theme button location

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -4,9 +4,11 @@ import { ThemeContext } from './contexts/ThemeContext';
 const ThemeToggle = () => {
   const { state, dispatch } = useContext(ThemeContext);
   return (
+    <div className='theme-container'>
       <button className="theme-btn" onClick={() => dispatch({ type: "TOGGLE_THEME" })}>
-        {state.theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+        {state.theme === 'Light Mode' ? 'Dark Mode' : 'Light Mode'}
       </button>
+    </div>
   );
 };
 export default ThemeToggle;
